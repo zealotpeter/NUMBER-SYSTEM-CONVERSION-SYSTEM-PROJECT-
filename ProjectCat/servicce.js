23383ab9f9fdd88ee6ac68595792653f6de8bb97
zealotpeter <zealotrajoh@gmail.com>
2025-09-26 11:21:47 +0300

    Add files via upload

---
+++ ProjectCat/servicce.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'number-converter-v1';
+const urlsToCache = [
+    './number_converter.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
