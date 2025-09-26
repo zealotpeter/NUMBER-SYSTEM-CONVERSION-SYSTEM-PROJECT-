66e45217369f281a5d2484647109683c31f57c69
zealotpeter <zealotrajoh@gmail.com>
2025-09-26 17:18:44 +0300

    Update servicce-worker.js

--- servicce-worker.js
+++ servicce-worker.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = 'number-converter-v1';
+const CACHE_NAME = "number-converter-v1";
 const urlsToCache = [
-    './number_converter.html',
-    './manifest.json'
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
